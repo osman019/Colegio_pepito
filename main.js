d9e898f08b744192ca499ab72883a3437fd026bc
osman019 <osmanortizrolon@gmail.com>
2024-09-04 16:59:41 -0500

    actualizacion

--- main.js
+++ main.js
@@ -13,6 +13,31 @@ const nombreInput = formData.get("nombre");
 const apellidoInput = formData.get("apellido");
 const edadInput = formData.get("edad");
 const idInput = formData.get("id");
+const emailInput = formData.get("email");
 
-const nuevoContacto = crearObjetoContacto
-(nombreInput, telefonoInput, emailInput);
+const nuevoestudiante = crearestudiante
+(nombreInput, apellidoInput, edadInput, idInput, emailInput);
+
+agregarestudiante(nuevoestudiante);
+
+
+
+function agregarestudiante(estudiantes) {
+    try {
+        estudiantes.push(estudiantes);
+        console.log("si se agrego: ", estudiantes);
+    }
+    catch (e){
+        console.error("no se ha agregado");
+    }
+}
+
+
+function crearestudiante(nombreE, apellidoE,idE, emailE){
+
+return {
+    nombre: nombreE,
+    apellido: apellidoE,
+    id: idE,
+    email: emailE
+}};
